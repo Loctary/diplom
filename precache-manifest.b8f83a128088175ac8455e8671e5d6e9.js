3d30b55988fd363556629388bf850858a1bb949a
loctary <39388080+Loctary@users.noreply.github.com>
2019-10-20 15:58:47 +0300

    Updates

---
+++ precache-manifest.b8f83a128088175ac8455e8671e5d6e9.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "273f361bd18e4a1c1b6817bed93a1a0d",
+    "url": "/diplom/index.html"
+  },
+  {
+    "revision": "23cf8597dadd6fa186f8",
+    "url": "/diplom/static/js/2.884a3e71.chunk.js"
+  },
+  {
+    "revision": "2ea268b3b66bc8ef27fe",
+    "url": "/diplom/static/js/main.32923d9f.chunk.js"
+  },
+  {
+    "revision": "fcaf4d60c7fe9c713db4",
+    "url": "/diplom/static/js/runtime-main.1739d328.js"
+  }
+]);
