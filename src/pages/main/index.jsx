34272bca553f7b45c3b5ac70025c07a5088beee8
loctary <39388080+Loctary@users.noreply.github.com>
2019-12-09 21:04:11 +0200

    parsing excels from ukrstat server implemented. not yet organized

--- src/pages/main/index.jsx
+++ src/pages/main/index.jsx
@@ -3,30 +3,67 @@ import Highcart from 'components/highchart';
 import XLSX from 'xlsx';
 
 const fetchData = async () => {
-  const response = await fetch('https://cors-anywhere.herokuapp.com/https://cors-anywhere.herokuapp.com/http://ukrstat.gov.ua/operativ/operativ2013/fin/kp_reg/kp_reg_u/xls_u/kp_reg_u_2018.xlsx');
+  const response = await fetch('https://cors-anywhere.herokuapp.com/http://ukrstat.gov.ua/operativ/operativ2013/fin/kp_reg/kp_reg_u/xls_u/kp_reg_u_2017.xlsx');
   // .then((byteStream) => byteStream.json())
   // .then((decodedData) => console.log(decodedData));
   return response;
 };
 
+function blobToFile(theBlob, fileName) {
+  // A Blob() is almost a File() - it's just missing the two properties below which we will add
+  theBlob.lastModifiedDate = new Date();
+  theBlob.name = fileName;
+  return theBlob;
+}
+
+
 class Main extends Component {
   async componentDidMount() {
     // fetchData();
     const stream = await fetchData();
-    const text = await stream.blob();
-    console.log(text);
-    // const reader = await stream.body.getReader();
-    // console.log(stream.body);
-    // const workbook = XLSX.read(stream);
-    // const buffers = [];
-    // stream.on('data', (data) => {
-    //   buffers.push(data);
-    // });
-    // stream.on('end', () => {
-    //   const buffer = Buffer.concat(buffers);
-    //   const workbook = XLSX.read(buffer); // works
+    const arrayBuffer = await stream.arrayBuffer();
+    const data = new Uint8Array(arrayBuffer);
+    const arr = new Array();
+    for (let i = 0; i != data.length; ++i) arr[i] = String.fromCharCode(data[i]);
+    const bstr = arr.join('');
+    const workbook = XLSX.read(bstr, { type: 'binary' });
+    const sheet = workbook.Sheets;
+    const sheetName = Object.keys(sheet)[0];
+    const json = XLSX.utils.sheet_to_json(workbook.Sheets[sheetName], { header: 1 });
+    console.log(json);
+
+
+    // reader.readAsDataURL(blob);
+    // reader.onloadend = function () {
+    //   const base64data = reader.result;
+    //   const workbook = XLSX.read(base64data, { raw: true });
     //   console.log(workbook);
-    // });
+    // };
+    // const file = new File([blob], 'kek.xlsx', { type: 'file', lastModified: Date.now() });
+    // console.log(file);
+    // const excel = XLSX.read(file);
+    // console.log(excel);
+
+    // const reader = new FileReader();
+    // reader.onload = function(event) {
+    // const data = event.target.result
+    // }
+    // console.log(blobToFile(blob, 'kek'));
+
+    // try {
+    //   this.readFile(file)
+    //     .then((textFile) => XLSX.read(textFile, { type: 'binary', cellDates: true }))
+    //     .then((res) => {
+    //       const { uploadedParsedFile, validationErrors } = this.handleUploadedExcel(res);
+    //       this.setState({
+    //         uploadedFile: file,
+    //         uploadedParsedFile,
+    //         validationErrors,
+    //         activeTabName: uploadedParsedFile.tabs[0].modelName,
+    //       });
+    //     });
+    // } catch (error) {
+    // }
   }
 
   render() {
@@ -39,3 +76,14 @@ class Main extends Component {
 }
 
 export default Main;
+
+
+// const stream = await fetchData();
+// const blob = await stream.blob();
+// const reader = new FileReader();
+// reader.readAsDataURL(blob);
+// reader.onloadend = function () {
+//   const base64data = reader.result;
+//   const workbook = XLSX.read(base64data, { raw: true });
+//   console.log(workbook);
+// };
